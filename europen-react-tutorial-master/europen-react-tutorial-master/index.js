bbed9a8da198454b28515856dce4cf7fa891ca47
majkyyx <hoze@ders.cz>
2015-10-04 17:42:29 +0200

    a

--- europen-react-tutorial-master/europen-react-tutorial-master/index.js
+++ europen-react-tutorial-master/europen-react-tutorial-master/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 //import './css/skeleton';
 
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import {todoApp} from './src/reducers';
 import {addTodo} from './src/actions';
-import {App} from './src/app';
+import App from './src/app';
 
 let store = createStore(todoApp);
 
@@ -17,11 +18,14 @@ const unsubscribe = store.subscribe(() => {
 console.log(store.getState());
 
 store.dispatch(addTodo('Need to learn redux'));
+store.dispatch(addTodo('Need to learn redux 2'));
 
 unsubscribe();
 
 let rootElement = document.getElementById('root');
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   rootElement
 );
--- europen-react-tutorial-master/europen-react-tutorial-master/src/app.js
+++ europen-react-tutorial-master/europen-react-tutorial-master/src/app.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
 import {TodoList} from './components';
 
-const todos = [{text: 'need to learn rect 2', completed: false}]; 
+// const todos = [{text: 'need to learn rect 2', completed: false}]; 
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <TodoList todos={todos} />
+        <TodoList todos={this.props.todos} />
       </div>
     )
   }
 }
+
+function select(state){
+  return {
+    todos: state.todos
+  }
+}
+
+export default connect(select)(App);
