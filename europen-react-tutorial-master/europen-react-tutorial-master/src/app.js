1ad35d8ee63ba2aa5c48c92cb891c52dbfd6ac7e
majkyyx <hoze@ders.cz>
2015-10-04 17:19:09 +0200

    test

---
+++ europen-react-tutorial-master/europen-react-tutorial-master/src/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import {TodoList} from './components';
+
+const todos = [{text: 'need to learn rect 2', completed: false}];
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoList todos={todos} />
+      </div>
+    )
+  }
+}
--- europen-react-tutorial-master/europen-react-tutorial-master/src/components.js
+++ europen-react-tutorial-master/europen-react-tutorial-master/src/components.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 
+class TodoItem extends Component {
+  render () {
+    cons {item} = this.props;
+    return (
+      <li>{item.text}</li>
+    )
+  }
+}
+
 export class TodoList extends Component{
   render() {
     consts todos = this.props.todos;
     return (
       <ul>
       {todos.map((todo, index) => {
-        return <TodoItem key={index} index={todo} />
+        return <TodoItem key={index} index={todo} /> 
       })}
       </ul>
     )
